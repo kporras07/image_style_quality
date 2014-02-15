6975153f133d79f9b5c34d98984cde2c9996c411
Sam <sam@1485048.no-reply.drupal.org>
2014-02-16 10:38:49 +1300

    Updated edit form to allow users to see existing quality on image style

--- lib/Drupal/image_style_quality/Plugin/ImageEffect/ImageStyleQualityImageEffect.php
+++ lib/Drupal/image_style_quality/Plugin/ImageEffect/ImageStyleQualityImageEffect.php
@@ -14,6 +14,7 @@ use Drupal\Core\Image\ImageInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+
 /**
  * Allows you to change the quality of an image, per image style.
  *
@@ -25,29 +26,46 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class ImageStyleQualityImageEffect extends ImageEffectBase implements ConfigurableImageEffectInterface, ContainerFactoryPluginInterface {
 
+  /**
+   * {@inheritdoc}
+   */
   public function applyEffect(ImageInterface $image) {
-
+    $quality = $this->configuration['image_jpeg_quality'];
+    $gd_config = $this->config_factory->get('system.image.gd');
+    // Override the config for the image quality, for this request only.
+    $gd_config->setModuleOverride(array(
+      'jpeg_quality' => $quality
+    ));
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getForm() {
+    $existing_quality = isset($this->configuration['image_jpeg_quality']) ? $this->configuration['image_jpeg_quality'] : FALSE;
     $form['image_jpeg_quality'] = array(
       '#type' => 'number',
       '#title' => t('JPEG quality'),
       '#description' => t('Define the image quality for JPEG manipulations. Ranges from 0 to 100. Higher values mean better image quality but bigger files.'),
       '#min' => 0,
       '#max' => 100,
-      '#default_value' => $this->config->get('system.image.gd')->get('jpeg_quality'),
+      '#default_value' => $existing_quality ?: $this->config_factory->get('system.image.gd')->get('jpeg_quality'),
       '#field_suffix' => t('%'),
     );
     return $form;
   }
 
-
+  /**
+   * {@inheritdoc}
+   */
   public function __construct(array $configuration, $plugin_id, array $plugin_definition, ConfigFactoryInterface $config) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->config = $config;
+    $this->config_factory = $config;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, array $plugin_definition) {
     return new static(
       $configuration,
