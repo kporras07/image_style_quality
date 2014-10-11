8c9ec4bcc24307baa093e4b502171cabac4d5fa4
Sam <sam@1485048.no-reply.drupal.org>
2014-10-11 18:22:13 +0800

    Updated to work with HEAD.

--- src/Plugin/ImageEffect/ImageStyleQualityImageEffect.php
+++ src/Plugin/ImageEffect/ImageStyleQualityImageEffect.php
@@ -2,16 +2,15 @@
 
 /**
  * @file
- * Contains \Drupal\image_style_quality\Plugin\ImageEffect\ImageStyleQualityImageEffect
+ * Contains \Drupal\image_style_quality\Plugin\ImageEffect\ImageStyleQualityImageEffect.
  */
 
 namespace Drupal\image_style_quality\Plugin\ImageEffect;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Image\ImageInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\image\ImageEffectBase;
-use Drupal\image\ConfigurableImageEffectInterface;
+use Drupal\image\ConfigurableImageEffectBase;
+use Drupal\Core\Form\FormStateInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -23,16 +22,21 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   description = @Translation("Allows you to change the quality of an image, per image style.")
  * )
  */
-class ImageStyleQualityImageEffect extends ImageEffectBase implements ConfigurableImageEffectInterface, ContainerFactoryPluginInterface {
+class ImageStyleQualityImageEffect extends ConfigurableImageEffectBase {
+
+  /**
+   * The GD image config object.
+   *
+   * @var null
+   */
+  var $gd_config = NULL;
 
   /**
    * {@inheritdoc}
    */
   public function applyEffect(ImageInterface $image) {
-    $quality = $this->configuration['image_jpeg_quality'];
-    $gd_config = $this->config_factory->get('system.image.gd');
-    // Override the config for the image quality, for this request only.
-    $gd_config->setModuleOverride(array(
+    $quality = $this->configuration['quality'];
+    $this->gd_config->setModuleOverride(array(
       'jpeg_quality' => $quality
     ));
   }
@@ -40,15 +44,15 @@ class ImageStyleQualityImageEffect extends ImageEffectBase implements Configurab
   /**
    * {@inheritdoc}
    */
-  public function getForm() {
-    $existing_quality = isset($this->configuration['image_jpeg_quality']) ? $this->configuration['image_jpeg_quality'] : FALSE;
-    $form['image_jpeg_quality'] = array(
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $existing_quality = isset($this->configuration['quality']) ? $this->configuration['quality'] : '75';
+    $form['quality'] = array(
       '#type' => 'number',
-      '#title' => t('JPEG quality'),
+      '#title' => t('Quality'),
       '#description' => t('Define the image quality for JPEG manipulations. Ranges from 0 to 100. Higher values mean better image quality but bigger files.'),
       '#min' => 0,
       '#max' => 100,
-      '#default_value' => $existing_quality ?: $this->config_factory->get('system.image.gd')->get('jpeg_quality'),
+      '#default_value' => $existing_quality,
       '#field_suffix' => t('%'),
     );
     return $form;
@@ -57,30 +61,38 @@ class ImageStyleQualityImageEffect extends ImageEffectBase implements Configurab
   /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ConfigFactoryInterface $config) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->config_factory = $config;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, array $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('config.factory')
-    );
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['quality'] = $form_state->getValue('quality');
   }
 
   /**
    * {@inheritdoc}
    */
   public function getSummary() {
-    $quality = $this->configuration['image_jpeg_quality'];
+    $quality = $this->configuration['quality'];
     return array(
       '#markup' => '(' . $quality . '% ' . $this->t('Quality') . ')',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerInterface $logger, $gd_config) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $logger);
+    $this->gd_config = $gd_config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory')->get('image'),
+      $container->get('config.factory')->get('system.image.gd')
     );
   }
 
